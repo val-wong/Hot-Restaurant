5a6278475c09c5162a7f6fc0855174675cde7c51
Val Wong <valentin.wong@gmail.com>
2017-07-31 23:23:55 -0700

    first commit to all files

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+  {
+    customerName: 'Val',
+    customerEmail: 'val@email.com',
+    customerID: 'val1981',
+    phoneNumber: '000-000-0000'
+  }
+];
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+  {
+    customerName: 'Reina',
+    customerEmail: 'reina@email.com',
+    customerID: 'reina1982',
+    phoneNumber: '000-000-0000'
+  }
+];
+
+module.exports = waitingArray;
